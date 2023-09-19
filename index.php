fddd08721764ba0ff0603725c0aec8c3828b8531
Thuan Nguyen <thuan@bitvillegaming.com>
2023-09-19 23:07:00 +0700

    h1 font and toast css update

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
 
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;900&display=swap"
+    <link href="https://fonts.googleapis.com/css2?family=Dosis:wght@700&family=Open+Sans:wght@400;900&display=swap"
           rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="bundle/styles.bundle.css?time=<?php echo time() ?>">
